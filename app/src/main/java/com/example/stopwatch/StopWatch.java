b5b1ca83e28a17994b20ec11d8cda3ee6a933ac2
Petriv2004 <felipetrivinogarzon@gmail.com>
2025-02-28 14:55:37 -0500

    Agregando archivos para la funcionalidad StopWatch

---
+++ app/src/main/java/com/example/stopwatch/StopWatch.java
@@ -0,0 +1,64 @@
+package com.example.stopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.Locale;
+
+public class StopWatch extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_stopwatch);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),"%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+
+    }
+}
